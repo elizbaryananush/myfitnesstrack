0a8ac218433828676736049e44a388c278292ece
elizbaryananush <elizbaryananush27@gmail.com>
2023-10-26 15:35:09 +0400

    initialcommit

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(compression())
 app.use(express.json())
 app.use(cors({
-    origin: ["https://myfitnesstrack-client.vercel.app" , "https://myfitnesstrack-server1.vercel.app/api/login"],
+    origin: ["https://myfitnesstrack-client.vercel.app" , "https://myfitnesstrack-server1.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
